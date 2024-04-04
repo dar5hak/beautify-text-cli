2f3d8f658a35ddab0ebe0fc581bbf9cd1e223c53
Darshak Parikh <darshak@coffeebeans.io>
2024-04-04 14:10:15 +0530

    Integrate Prettier

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 #!/usr/bin/env node
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
-const beautifyText = require('beautify-text');
-const glob = require('glob');
-const through2 = require('through2');
+const beautifyText = require("beautify-text");
+const glob = require("glob");
+const through2 = require("through2");
 
 if (process.argv.length > 2) {
-	process.argv
-		.slice(2)
-		.map(pattern => glob.sync(pattern)) // Extract matched filenames
-		.reduce((acc, curr) => acc.concat(curr)) // Concat all arrays into one
-		// TODO: Deduplicate
-		// .reduce((acc, curr) => acc.includes(curr) ? acc : acc.concat(curr))
-		.forEach(filename => {
-			const fileReadStream = fs.createReadStream(filename);
-			beautifyStream(fileReadStream);
-		});
+  process.argv
+    .slice(2)
+    .map((pattern) => glob.sync(pattern)) // Extract matched filenames
+    .reduce((acc, curr) => acc.concat(curr)) // Concat all arrays into one
+    // TODO: Deduplicate
+    // .reduce((acc, curr) => acc.includes(curr) ? acc : acc.concat(curr))
+    .forEach((filename) => {
+      const fileReadStream = fs.createReadStream(filename);
+      beautifyStream(fileReadStream);
+    });
 } else {
-	beautifyStream(process.stdin);
+  beautifyStream(process.stdin);
 }
 
 function beautifyStream(stream) {
-	const beautify = through2(function (chunk, enc, callback) {
-		this.push(beautifyText(chunk.toString()));
-		callback();
-	});
-	stream
-		.pipe(beautify)
-		.pipe(process.stdout);
+  const beautify = through2(function (chunk, enc, callback) {
+    this.push(beautifyText(chunk.toString()));
+    callback();
+  });
+  stream.pipe(beautify).pipe(process.stdout);
 }
--- test/test.js
+++ test/test.js
@@ -1,39 +1,39 @@
-const test = require('ava');
-const nixt = require('nixt');
+const test = require("ava");
+const nixt = require("nixt");
 
-test.cb('stdin via pipe', t => {
-	nixt()
-		.run('echo "Hello world" | beautify')
-		.stdout('“Hello world”')
-		.end(() => {
-			t.end();
-		});
+test.cb("stdin via pipe", (t) => {
+  nixt()
+    .run('echo "Hello world" | beautify')
+    .stdout("“Hello world”")
+    .end(() => {
+      t.end();
+    });
 });
 
-test.cb('interactive stdin', t => {
-	nixt()
-		.run('beautify')
-		.stdin('"Hello world"')
-		.stdout('“Hello world”')
-		.end(() => {
-			t.end();
-		});
+test.cb("interactive stdin", (t) => {
+  nixt()
+    .run("beautify")
+    .stdin('"Hello world"')
+    .stdout("“Hello world”")
+    .end(() => {
+      t.end();
+    });
 });
 
-test.cb('single file', t => {
-	nixt()
-		.run('beautify test1.txt')
-		.stdout('“Hello world”')
-		.end(() => {
-			t.end();
-		});
+test.cb("single file", (t) => {
+  nixt()
+    .run("beautify test1.txt")
+    .stdout("“Hello world”")
+    .end(() => {
+      t.end();
+    });
 });
 
-test.cb('glob', t => {
-	nixt()
-		.run('beautify test*.txt')
-		.stdout('“Hello world\nWait — ermm…”')
-		.end(() => {
-			t.end();
-		});
+test.cb("glob", (t) => {
+  nixt()
+    .run("beautify test*.txt")
+    .stdout("“Hello world\nWait — ermm…”")
+    .end(() => {
+      t.end();
+    });
 });
